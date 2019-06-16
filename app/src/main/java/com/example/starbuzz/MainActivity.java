5f17954867646d47f9b5d51cd622f0982c41987f
Viktorgit commit -m 'first commit'git config --global user.name Viktor <viktor.hurtovyi@gmail.com>
2019-06-16 14:34:41 +0300

    'coffee'

---
+++ app/src/main/java/com/example/starbuzz/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.starbuzz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Создать OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    public void onItemClick(AdapterView<?> listView,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        if (position == 0) {
+                            Intent intent = new Intent(MainActivity.this,
+                                    DrinkCategoryActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                };
+        //Добавить слушателя к списковому представлению
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
+    }
+}
